b73d5dc7866164eb994540d92f9b21c597a20e4c
D5n9sMatrix <denis.02live@hotmail.com>
2023-06-09 12:19:48 -0300

    welcome Jason Voorhees Math

---
+++ build/CMakeFiles/jason-voorhees.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/jason-voorhees.dir/jason-voorhees.cpp.o"
+  "CMakeFiles/jason-voorhees.dir/jason-voorhees.cpp.o.d"
+  "libjason-voorhees.a"
+  "libjason-voorhees.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/jason-voorhees.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/denis/freebasic.desk/Jason-Voorhees
+# Build directory: /home/denis/freebasic.desk/Jason-Voorhees/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
